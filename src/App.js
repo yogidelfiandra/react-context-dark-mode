83b6de20038405412be61f0badfbc3731e707d5b
Yogi Delfiandra <yogi.delfiandra03@gmail.com>
2023-05-28 15:09:07 +0700

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,51 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import ToggleTheme from './components/ToggleTheme';
+import { ThemeProvider } from './contexts/ThemeContext';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      theme: localStorage.getItem('theme') || 'light',
+      toggleTheme: () => {
+        this.setState((prevState) => {
+          // mendapatkan nilai tema baru berdasarkan state sebelumnya
+          const newTheme = prevState.theme === 'light' ? 'dark' : 'light';
+          // menyimpan nilai tema baru ke local storage
+          localStorage.setItem('theme', newTheme);
+
+          // mengembalikan state dengan nilai theme terbaru.
+          return {
+            theme: newTheme,
+          };
+        });
+      },
+    };
+  }
+
+  componentDidMount() {
+    document.documentElement.setAttribute('data-theme', this.state.theme);
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    if (prevState.theme !== this.state.theme) {
+      document.documentElement.setAttribute('data-theme', this.state.theme);
+    }
+  }
+
+  render() {
+    return (
+      <ThemeProvider value={this.state}>
+        <div className='container'>
+          <ToggleTheme />
+          <p>
+            Hello, {this.state.theme === 'light' ? 'Early Bird' : 'Night Owl'}.
+          </p>
+        </div>
+      </ThemeProvider>
+    );
+  }
 }
 
 export default App;
